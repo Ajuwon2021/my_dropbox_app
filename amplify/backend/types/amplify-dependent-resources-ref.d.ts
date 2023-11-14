9bd897fa7bbda8b051f4d63d1144d27ae86cd890
Ajuwon Ayodeji Folarin <96354317+Ajuwon2021@users.noreply.github.com>
2023-11-14 08:06:35 +0100

    S3 and lambda commits

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Triggerb18cd2ed": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dropboxstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,53 +1,40 @@
-// FileUpload.js
-import React, { useCallback, useState } from "react";
-import { useDropzone } from "react-dropzone";
+import React, { useState } from "react";
 import { Storage } from "aws-amplify";
-import { toast } from "react-toastify";
+import { Button, Input, Progress } from "antd";
 
 const FileUpload = () => {
+  const [file, setFile] = useState(null);
   const [uploadProgress, setUploadProgress] = useState(0);
 
-  const onDrop = useCallback(async (acceptedFiles) => {
-    const file = acceptedFiles[0];
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
 
-    try {
-      const result = await Storage.put(file.name, file, {
-        progressCallback(progress) {
-          const percentage = (progress.loaded / progress.total) * 100;
-          setUploadProgress(percentage);
-        },
-      });
-
-      console.log("File uploaded successfully:", result);
-
-      // You can add additional logic here, e.g., update UI, trigger further actions, etc.
-
-      toast.success("File uploaded successfully!");
-    } catch (error) {
-      console.error("Error uploading file:", error);
-      toast.error("Error uploading file. Please try again.");
-    } finally {
-      // Reset the upload progress after the upload is complete or encounters an error
-      setUploadProgress(0);
+  const uploadFile = async () => {
+    if (file) {
+      try {
+        const result = await Storage.put(file.name, file, {
+          progressCallback(progress) {
+            const percent = Math.round(
+              (progress.loaded / progress.total) * 100
+            );
+            setUploadProgress(percent);
+          },
+        });
+        console.log("File uploaded successfully", result);
+      } catch (error) {
+        console.error("Error uploading file", error);
+      }
     }
-  }, []);
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  };
 
   return (
     <div>
-      <div
-        {...getRootProps()}
-        className={`dropzone ${isDragActive ? "active" : ""}`}
-      >
-        <input {...getInputProps()} />
-        <p>Drag & drop files here, or click to select files</p>
-      </div>
+      <Input type="file" onChange={handleFileChange} />
+      <Button onClick={uploadFile}>Upload</Button>
       {uploadProgress > 0 && (
-        <div>
-          <p>Uploading: {uploadProgress.toFixed(2)}%</p>
-          <progress value={uploadProgress} max="100" />
-        </div>
+        <Progress percent={uploadProgress} status="active" />
       )}
     </div>
   );
